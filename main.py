f99b796818b3257d75c5c6daa84eed054237add8
hsanders-07 <hsander7@byu.edu>
2024-11-20 20:32:49 -0700

    slight edits to libraries

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import zipfile
-import plotly.express as px
+#import plotly.express as px
 import matplotlib.pyplot as plt
 import requests
 from io import BytesIO
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+#import plotly.graph_objects as go
+#from plotly.subplots import make_subplots
 from my_plots import *
 import streamlit as st
 
